203776016b12d6c0dccee5b379af4ec945b78ac8
SRINIVAS KALYAN <90304255+KALYAN1045@users.noreply.github.com>
2023-04-27 23:50:52 +0530

    Removed sections that are un-necessary

--- user_page.php
+++ user_page.php
@@ -44,57 +44,6 @@
     </section>
     <!-- home sections ends -->
 
-    <!-- service section starts -->
-
-    <!-- <section class="services" id="services">
-        <h1 class="heading"> our <span>services</span></h1>
-        <div class="box-container">
-            <div class="box">
-                <i class="fas fa-notes-medical"></i>
-                <h3>medical camps</h3>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio, tempora?</p>
-                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span></a>
-            </div>
-
-            <div class="box">
-                <i class="fas fa-ambulance"></i>
-                <h3>24/7 ambulance</h3>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio, tempora?</p>
-                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span></a>
-            </div>
-
-            <div class="box">
-                <i class="fas fa-user-md"></i>
-                <h3>Online consultation</h3>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio, tempora?</p>
-                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span></a>
-            </div>
-
-            <div class="box">
-                <i class="fas fa-tablets"></i>
-                <h3>medicines delivery</h3>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio, tempora?</p>
-                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span></a>
-            </div>
-
-            <div class="box">
-                <i class="fas fa-file-medical-alt"></i>
-                <h3>Emergency bookings</h3>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio, tempora?</p>
-                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span></a>
-            </div>
-
-            <div class="box">
-                <i class="fas fa-hand-holding-medical"></i>
-                <h3>total care</h3>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio, tempora?</p>
-                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span></a>
-            </div>
-        </div>
-    </section> -->
-
-    <!-- service section ends -->
-
     <!-- icon section starts -->
     <section class="icons-container">
         <div class="icons">
